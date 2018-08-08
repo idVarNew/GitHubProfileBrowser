68f6593623eb09ef819c1da200404004f53a95f9
Ewelina Brus <idvarnew@gmail.com>
2018-08-09 00:19:34 +0200

    Fix bug causing not loading data

--- src/app.js
+++ src/app.js
@@ -1,29 +1,39 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
+import 'es6-promise/auto'
+import 'isomorphic-fetch'
 
 
 export class App {
-  initializeApp() {
-    let self = this;
 
-    $('.load-username').on('click', function (e) {
-      let userName = $('.username.input').val();
-
-      fetch('https://api.github.com/users/' + userName)
-        .then((response)=> {response.json})
-        .then(function (body) {
-          self.profile = body;
-          self.update_profile();
-        })
-
-    })
-
+  constructor() {
+    this.initializeApp();
+    this.usernameInput = $('.username.input')
   }
 
-  update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
+  initializeApp() {
+
+    $('.load-username').on('click', (e) => {
+      const userName = this.usernameInput.val()
+
+      fetch('https://api.github.com/users/' + userName)
+        .then((response) => {
+          if (!response.ok) throw Error(response.statusText);
+          return response.json()
+        })
+        .then((profile) => {
+          this.updateProfile(profile)
+        }).catch((error) => {
+          console.log(error);
+        })
+    })
+  }
+
+
+  updateProfile(profile) {
+    $('#profile-name').text(this.usernameInput.val())
+    $('#profile-image').attr('src', profile.avatar_url)
+    $('#profile-url').attr('href', profile.html_url).text(profile.login)
+    $('#profile-bio').text(profile.bio || '(no information)')
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import styles from './assets/scss/app.scss';
 import {App} from "./app";
 
 const app = new App();
 
-app.initializeApp();
