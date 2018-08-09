74af3eaf19b995c00bc37e724f1f29190ed05cfd
Ewelina Brus <idvarnew@gmail.com>
2018-08-09 02:05:27 +0200

    Add validation to input

--- src/app.js
+++ src/app.js
@@ -7,31 +7,66 @@ import 'isomorphic-fetch'
 export class App {
 
   constructor() {
+
+    this.userNameInput = $('.username.input');
+    this.loadButton = $('.load-username');
+    this.errorOnValidation = false;
     this.initializeApp();
-    this.usernameInput = $('.username.input')
   }
 
   initializeApp() {
 
-    $('.load-username').on('click', (e) => {
-      const userName = this.usernameInput.val()
+    this.loadButton.on('click', (e) => {
+      e.preventDefault();
+      
+      const userName = this.userNameInput.val();
 
-      fetch('https://api.github.com/users/' + userName)
-        .then((response) => {
-          if (!response.ok) throw Error(response.statusText);
-          return response.json()
-        })
-        .then((profile) => {
-          this.updateProfile(profile)
-        }).catch((error) => {
-          console.log(error);
-        })
+      if (userName.match(/^[a-z0-9_-]+$/)) {
+
+        this.errorOnValidation = false;
+
+        if (!this.errorOnValidation) {
+          this.removeErrorSign()
+        }
+
+        this.getUserProfile(userName);
+
+      } else {
+        this.addErrorSign("red");
+        this.errorOnValidation = true;
+      }
     })
   }
 
+  getUserProfile(userName) {
+    fetch('https://api.github.com/users/' + userName)
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((profile) => {
+        this.updateProfile(profile)
+      }).catch((error) => {
+        console.log(error);
+      })
+  }
+
+  addErrorSign() {
+    this.userNameInput.addClass("is-validation-error");
+    console.log(`
+     Input value is invalid. Username name can only contains lowercase letter, numbers and _ -. Input cannot be empty.
+    `)
+  }
+
+  removeErrorSign() {
+    this.userNameInput.removeClass("is-validation-error");
+    console.log(`
+    Input value is invalid. Username name can only contains lowercase letter, numbers and _ -. Input cannot be empty.
+  `)
+  }
 
   updateProfile(profile) {
-    $('#profile-name').text(this.usernameInput.val())
+    $('#profile-name').text(profile.login)
     $('#profile-image').attr('src', profile.avatar_url)
     $('#profile-url').attr('href', profile.html_url).text(profile.login)
     $('#profile-bio').text(profile.bio || '(no information)')
